3645b921564548344862bd111b2bfb4cfbfcbd85
basia brzeziecka <basia.brzeziecka@gmail.com>
2024-08-03 15:53:24 +0100

    added playground page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :playground ]
 
   def home
     @show_navbar = false
+  end
+
+  def playground
   end
 end
