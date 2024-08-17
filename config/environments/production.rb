d5b1d437df7e22773ca88160a3a563208d4a0eab
Jules Pinto <pintojules@gmail.com>
2024-08-17 10:37:57 +0100

    added redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
+  config.cache_store = :redis_cache_store,{url: ENV['REDIS_URL']}
+
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
